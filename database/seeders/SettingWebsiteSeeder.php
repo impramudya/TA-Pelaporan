449f4c2e6b366bfbdd7a4814d9d23e8773891eb5
pramudya <itsme.pramudya@gmail.com>
2024-03-07 01:51:33 +0700

    edit

--- database/seeders/SettingWebsiteSeeder.php
+++ database/seeders/SettingWebsiteSeeder.php
@@ -20,7 +20,7 @@ class SettingWebsiteSeeder extends Seeder
             ['key' => 'IMAGE_WEB_LOGO_WHITE', 'value' => 'logoT.png', "flag_active" => "Y"],
             ['key' => 'IMAGE_WEB_LOGO', 'value' => 'logo-dashboard.png', "flag_active" => "Y"],
             ['key' => 'IMAGE_WEB_FAVICON', 'value' => 'logo.png', "flag_active" => "Y"],
-            ['key' => 'TEXT_HERO_HEADER', 'value' => 'Sistem Pengakuan Online SMKN 4 Kota Tangerang', "flag_active" => "Y"],
+            ['key' => 'TEXT_HERO_HEADER', 'value' => 'Sistem Pengakuan Online SMA Negeri 1 Cikampek', "flag_active" => "Y"],
             ['key' => 'TEXT_HERO_DESCRIPTION', 'value' => 'Sampaikan pengakuan, laporan, kritik, atau saran kamu di sini~', "flag_active" => "Y"],
             ['key' => 'IMAGE_FOOTER', 'value' => 'logo-smkn-4.png', "flag_active" => "Y"],
             ['key' => 'TEXT_FOOTER', 'value' => 'SMK Negeri 4 Tangerang', "flag_active" => "Y"],
